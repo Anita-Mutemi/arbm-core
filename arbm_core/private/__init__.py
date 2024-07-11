7d5a1ad3df9e41c6fd06043d7bfaf596cc4e71f6
Anita-Mutemi <anitajehu13@gmail.com>
2024-07-12 00:55:59 +0300

    update

---
+++ arbm_core/private/__init__.py
@@ -0,0 +1,46 @@
+from dataclasses import dataclass
+import datetime
+import os
+from uuid import UUID
+
+from sqlalchemy import create_engine, Column, Boolean
+from sqlalchemy.orm import declarative_base, sessionmaker
+
+from .. import BooleanModel
+
+Base = declarative_base()
+
+
+class Deletable:
+    removed = Column(Boolean, nullable=False, default=False)
+
+
+try:
+    db_host = os.environ.get("DB_HOST", "localhost")
+    db_name = os.environ.get("DB_NAME", "postgres")
+    db_user = os.environ.get("DB_USER", "postgres")
+    db_password = os.environ.get("DB_PASS", "0714676921Akm.")
+    db_port = os.environ.get("DB_PORT", 5432)
+    echo = BooleanModel(enable_echo=os.environ.get("ECHO", False)).enable_echo
+
+    engine = create_engine(
+        f"postgresql://{db_user}:{db_password}@{db_host}:{db_port}/{db_name}?sslmode=require",
+        echo=echo,
+        future=True,
+    )
+    Session = sessionmaker(engine, future=True)
+
+except KeyError as e:
+    raise RuntimeError(
+        f"Required environment variable {e} is not set. Please set it before running the application."
+    )
+
+
+@dataclass(eq=True, frozen=True)
+class SignalSchema:
+    date: datetime.date
+    fund_uuid: UUID
+    project_uuid: UUID
+
+
+from . import projects, relationships, twitter, users
