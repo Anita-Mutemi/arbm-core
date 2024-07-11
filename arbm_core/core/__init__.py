7f8818d6954e33412694bdda5426afe2f3881df5
Anita-Mutemi <anitajehu13@gmail.com>
2024-07-12 02:10:31 +0300

    update db

--- arbm_core/core/__init__.py
+++ arbm_core/core/__init__.py
@@ -6,5 +6,5 @@ __all__ = ["MongoDb"]
 config = dotenv_values(".env")
 
 
-_mongodb_client = MongoClient(config["MONGO_URI"], uuidRepresentation="standard")
-MongoDb = _mongodb_client[config["MONGO_DB_NAME"]]
+_mongodb_client = MongoClient(config["MONGO_ATLAS_URI"], uuidRepresentation="standard")
+MongoDb = _mongodb_client[config["MONGO_ATLAS_DB_NAME"]]
